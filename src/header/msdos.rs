b941ad41504a32a146093d6bb62312f4311cc8cb
Rafal Michalski <royaltm75@gmail.com>
2026-07-15 01:36:49 +0200

    msdos: add methods and Display implementation + tests

--- src/header/msdos.rs
+++ src/header/msdos.rs
@@ -1,7 +1,10 @@
+//! MS-DOS attributes
+use core::fmt;
 use bitflags::bitflags;
 
 bitflags! {
     #[derive(Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
+    /// MS-DOS attributes
     pub struct MsDosAttrs: u16 {
         const READ_ONLY = 0b00000000_00000001;
         const HIDDEN    = 0b00000000_00000010;
@@ -11,5 +14,107 @@ bitflags! {
         const ARCHIVE   = 0b00000000_00100000;
         const SYMLINK   = 0b00000000_01000000;
         const RESERVED  = 0b11111111_10000000;
+    }
+}
+
+impl MsDosAttrs {
+    /// Return whether a read-only flag is set.
+    pub fn is_read_only(self) -> bool {
+        self.intersects(MsDosAttrs::READ_ONLY)
+    }
+    /// Return whether a hidden flag is set.
+    pub fn is_hidden(self) -> bool {
+        self.intersects(MsDosAttrs::HIDDEN)
+    }
+    /// Return whether a system flag is set.
+    pub fn is_system(self) -> bool {
+        self.intersects(MsDosAttrs::SYSTEM)
+    }
+    /// Return whether a system flag is set.
+    pub fn is_volume(self) -> bool {
+        self.intersects(MsDosAttrs::VOLUME)
+    }
+    /// Return whether a system flag is set.
+    pub fn is_subdir(self) -> bool {
+        self.intersects(MsDosAttrs::SUBDIR)
+    }
+    /// Return whether an archive flag is set.
+    pub fn is_archive(self) -> bool {
+        self.intersects(MsDosAttrs::ARCHIVE)
+    }
+    /// Return whether a symlink flag is set.
+    pub fn is_symlink(self) -> bool {
+        self.intersects(MsDosAttrs::SYMLINK)
+    }
+}
+
+impl fmt::Display for MsDosAttrs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut flags: [u8;5] = *b"ADSHR";
+        let mask: usize = const {
+                    MsDosAttrs::READ_ONLY.
+            r#union(MsDosAttrs::HIDDEN).
+            r#union(MsDosAttrs::SYSTEM).
+            r#union(MsDosAttrs::SUBDIR).
+            r#union(MsDosAttrs::ARCHIVE)
+            .bits()
+        }.into();
+        let mut bit: usize = MsDosAttrs::ARCHIVE.bits().into();
+        let perm: usize = self.bits().into();
+        for ch in flags.iter_mut() {
+            if (mask & bit) == 0 {
+                bit >>= 1;
+            }
+            if (perm & bit) == 0 {
+                *ch = b'-';
+            }
+            bit >>= 1;
+        }
+        let flags = str::from_utf8(&flags).unwrap();
+        flags.fmt(f)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn msdos_attributes_works() {
+        assert!(MsDosAttrs::READ_ONLY.is_read_only());
+        assert!(MsDosAttrs::HIDDEN.is_hidden());
+        assert!(MsDosAttrs::SYSTEM.is_system());
+        assert!(MsDosAttrs::VOLUME.is_volume());
+        assert!(MsDosAttrs::SUBDIR.is_subdir());
+        assert!(MsDosAttrs::ARCHIVE.is_archive());
+        assert!(MsDosAttrs::SYMLINK.is_symlink());
+        assert!(!MsDosAttrs::empty().is_read_only());
+        assert!(!MsDosAttrs::empty().is_hidden());
+        assert!(!MsDosAttrs::empty().is_system());
+        assert!(!MsDosAttrs::empty().is_volume());
+        assert!(!MsDosAttrs::empty().is_subdir());
+        assert!(!MsDosAttrs::empty().is_archive());
+        assert!(!MsDosAttrs::empty().is_symlink());
+        assert_eq!(format!("{}", MsDosAttrs::READ_ONLY), "----R");
+        assert_eq!(format!("{}", MsDosAttrs::HIDDEN),    "---H-");
+        assert_eq!(format!("{}", MsDosAttrs::SYSTEM),    "--S--");
+        assert_eq!(format!("{}", MsDosAttrs::VOLUME),    "-----");
+        assert_eq!(format!("{}", MsDosAttrs::SUBDIR),    "-D---");
+        assert_eq!(format!("{}", MsDosAttrs::ARCHIVE),   "A----");
+        assert_eq!(format!("{}", MsDosAttrs::SYMLINK),   "-----");
+        assert_eq!(format!("{}", MsDosAttrs::empty()),   "-----");
+        assert_eq!(format!("{}", MsDosAttrs::all()),     "ADSHR");
+        assert_eq!(format!("{}", MsDosAttrs::all()),     "ADSHR");
+        assert_eq!(format!("{}", MsDosAttrs::READ_ONLY|
+                                 MsDosAttrs::HIDDEN|
+                                 MsDosAttrs::SYSTEM|
+                                 MsDosAttrs::SUBDIR|
+                                 MsDosAttrs::ARCHIVE),   "ADSHR");
+        assert_eq!(format!("{}", MsDosAttrs::READ_ONLY|
+                                 MsDosAttrs::ARCHIVE),   "A---R");
+        assert_eq!(format!("{}", MsDosAttrs::HIDDEN|
+                                 MsDosAttrs::SYSTEM|
+                                 MsDosAttrs::SUBDIR),    "-DSH-");
     }
 }
