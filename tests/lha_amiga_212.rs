254e0a3bf09e251626e7491af8e4534c9252f5d2
Rafal Michalski <royaltm75@gmail.com>
2026-07-20 17:23:44 +0200

    tests: morphos_lha_2717 from lhasa

--- tests/lha_amiga_212.rs
+++ tests/lha_amiga_212.rs
@@ -22,44 +22,42 @@ fn test_lha_amiga_212() -> io::Result<()> {
     for (name, path, size_c, size_o, crc16, crc32, modif, level, compr) in TESTS_CASES {
         println!("-------------\n{:?}", name);
         let mut lha_reader = delharc::parse_file(format!("tests/lha_amiga_212/{}", name))?;
-        for filen in 0.. {
-            assert!(filen <= 0);
-            let mut sink = SinkSum::new();
-            let header = lha_reader.header();
-            assert_eq!(header.level, *level);
-            assert_eq!(header.msdos_attrs, MsDosAttrs::HIDDEN);
-            assert_eq!(header.compression_method().unwrap(), *compr);
-            assert_eq!(header.compressed_size, *size_c);
-            assert_eq!(header.original_size, *size_o);
-            let path1 = path.replace("*", &std::path::MAIN_SEPARATOR.to_string());
-            assert_eq!(&header.parse_pathname().to_str().unwrap(), &path1);
-            let path1 = path.replace("*", "/");
-            assert_eq!(&header.parse_pathname_to_str(), &path1);
-            assert!(header.parse_comment().is_none());
-            assert!(!header.is_directory());
-            let last_modified = format!("{}", header.parse_last_modified());
-            assert_eq!(&last_modified, modif);
-            assert_eq!(header.file_crc, *crc16);
-            if header.level == 0 {
-                assert_eq!(header.parse_os_type()?, OsType::Generic);
-            }
-            else {
-                assert_eq!(header.parse_os_type()?, OsType::Amiga);
-            }
-            if *compr == CompressionMethod::Lhd {
-                assert!(io::copy(&mut lha_reader, &mut sink).is_err());
-            }
-            else {
-                io::copy(&mut lha_reader, &mut sink)?;
-            }
-            assert_eq!(sink.length, *size_o as u64);
-            assert_eq!(sink.crc32.get_crc(), *crc32);
-            assert_eq!(sink.crc16.get_crc(), *crc16);
-            assert_eq!(lha_reader.crc_check().unwrap(), *crc16);
-            if !lha_reader.next_file().unwrap() {
-                break;
-            }
+        let mut sink = SinkSum::new();
+        let header = lha_reader.header();
+        assert_eq!(header.level, *level);
+        assert_eq!(header.msdos_attrs, MsDosAttrs::HIDDEN);
+        assert_eq!(header.compression_method().unwrap(), *compr);
+        assert_eq!(header.compressed_size, *size_c);
+        assert_eq!(header.original_size, *size_o);
+        let path1 = path.replace("*", &std::path::MAIN_SEPARATOR.to_string());
+        assert_eq!(&header.parse_pathname().to_str().unwrap(), &path1);
+        let path1 = path.replace("*", "/");
+        assert_eq!(&header.parse_pathname_to_str(), &path1);
+        assert!(header.parse_os_9_attrs().is_none());
+        assert!(header.parse_unix_permissions().is_none());
+        assert!(header.parse_unix_uid_gid().is_none());
+        assert!(header.parse_comment().is_none());
+        assert!(!header.is_directory());
+        let last_modified = format!("{}", header.parse_last_modified());
+        assert_eq!(&last_modified, modif);
+        assert_eq!(header.file_crc, *crc16);
+        if header.level == 0 {
+            assert_eq!(header.parse_os_type()?, OsType::Generic);
         }
+        else {
+            assert_eq!(header.parse_os_type()?, OsType::Amiga);
+        }
+        if *compr == CompressionMethod::Lhd {
+            assert!(io::copy(&mut lha_reader, &mut sink).is_err());
+        }
+        else {
+            io::copy(&mut lha_reader, &mut sink)?;
+        }
+        assert_eq!(sink.length, *size_o as u64);
+        assert_eq!(sink.crc32.get_crc(), *crc32);
+        assert_eq!(sink.crc16.get_crc(), *crc16);
+        assert_eq!(lha_reader.crc_check().unwrap(), *crc16);
+        assert!(!lha_reader.seek_next_file().unwrap());
     }
     Ok(())
 }
